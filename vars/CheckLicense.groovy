c1d3479158bda1a7e168ffa77f44288f0d31fdb1
davrodgon <davrodgon@users.noreply.github.com>
2020-05-20 14:19:05 +0200

    Created License class

---
+++ vars/CheckLicense.groovy
@@ -0,0 +1,22 @@
+#!/usr/bin/groovy
+import eu.indigo.sqa.*
+
+/**
+ * Checks the license of a GitHub repository.
+ *
+ * @param owner The owner of the repository [mandatory]
+ * @param repository [mandatory]
+ */
+def call(String owner, String repository) {
+    String GITHUB_API = 'https://api.github.com/repos'
+    String url = "${GITHUB_API}/${owner}/${repository}/license"
+    try {
+       def jsonText = url.toURL().getText()
+       def json = new JsonSlurper().parseText(jsonText)
+       def spdx_id = json.license.spdx_id
+       def license = License.getLicenseData(spdx_id)
+       println "Is OSI approved ${license.isOsiApproved}"
+    } catch(FileNotFoundException e) {
+      println "License not found!"
+   }
+}
